b7cad507ef72167a2c12eb4b86fcdc4ad2db81ca
Antonio Anerão <anerao.junior@gmail.com>
2021-07-11 12:32:52 -0500

    Nova index

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app-name' => env('APP_NAME'),
+        'app-version' => env('APP_VERSION')
+    ]);
 });
